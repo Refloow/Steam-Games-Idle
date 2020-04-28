6a86f0f3d585cd7dedf4037af7007474f4f60d6a
Refloow <59449926+Refloow@users.noreply.github.com>
2020-04-28 04:02:15 +0200

    Updated to version 1.2.0
    - Added error checker for modules installed
    - Added detailed comments in code

--- app/steam.js
+++ app/steam.js
@@ -1,3 +1,24 @@
-const SteamUser = require('steam-user');
+// Steam-Donation-Bot-PRO - Bot built by Refloow (-MajokingGames)
+
+/* 
+  Here is contact info: refloowcontact@gmail.com 
+  or main dev steam: https://steamcommunity.com/id/MajokingGames/
+
+ */
+
+// Checking if all required modules are installed.
+try {
+        // Checking if steam-user is installed
+        SteamUser = require('steam-user');
+        // Checking if steam-totp module is installed
+        SteamTOTP = require('steam-totp');
+
+} catch (ex) {
+    // If modules are not installed showing an clear error message to user.
+   console.log('| [Modules] |: Missing dependencies. Install a version with dependecies or use npm install.');
+   console.log(ex);
+   process.exit(1);
+}
+
+// Setting client name
 const refloowidle = new SteamUser();
-const SteamTOTP = require('steam-totp');
