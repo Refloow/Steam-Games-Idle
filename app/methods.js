bb2b5df801a80c073776164c9061de3ac5c37e79
Refloow <59449926+Refloow@users.noreply.github.com>
2020-04-30 06:45:15 +0200

    Updated to 1.0.2

--- app/methods.js
+++ app/methods.js
@@ -1,7 +1,7 @@
 // Steam-Games-Idle - Bot built by Refloow (-MajokingGames)
 
 /* 
-  Here is contact info: refloowcontact@gmail.com 
+  Here is contact info: refloowlibrarycontact@gmail.com
   or main dev steam: https://steamcommunity.com/id/MajokingGames/
 
  */
@@ -60,7 +60,7 @@ t = module.exports = {
 // Steam-Games-Idle - Bot built by Refloow (-MajokingGames)
 
 /* 
-  Here is contact info: refloowcontact@gmail.com 
+  Here is contact info: refloowlibrarycontact@gmail.com
   or main dev steam: https://steamcommunity.com/id/MajokingGames/
 
  */
