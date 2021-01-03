e90e407be02b5fca17de25a4e6926b8913adbe82
Veljko Vuckovic <59449926+Refloow@users.noreply.github.com>
2021-01-03 01:17:42 +0100

    Added note

--- app/app.js
+++ app/app.js
@@ -89,12 +89,25 @@ refloowidle.on('loggedOn', () => {
 
 
 // Steam-Games-Idle - Bot built by Refloow (-MajokingGames)
+	
+	    
+/*
+
+Want active support and new updates with new features all for free?
+
+Leave an star on github repo its free ( we push updates based on the engagement )
+Repo link: https://github.com/OSL-Works/Steam-Games-Idle 
+
+  Discord Support Server: https://discord.gg/D8WCtDD     (Open an ticket)
+
+*/
+	
 
 /* 
-  Here is contact info: refloowlibrarycontact@gmail.com 
+  Here is contact info: refloowlibrarycontact@gmail.com
   main dev steam: https://steamcommunity.com/id/MajokingGames/
-  Support Discord: https://discord.gg/nNSDPvKBjt
   
   Donate: https://ko-fi.com/refloow
 
  */
+
