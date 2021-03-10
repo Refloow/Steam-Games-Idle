693c5d0e5accc839aad4e20948952a8e17c4af52
Veljko Vuckovic <59449926+Refloow@users.noreply.github.com>
2021-03-10 18:52:23 +0100

    Added notes

--- app/methods.js
+++ app/methods.js
@@ -1,26 +1,45 @@
-// Steam-Games-Idle - Bot built by Refloow (-MajokingGames)
-	
-	    
-/*
 
-Want active support and new updates with new features all for free?
+// Copyright notice:
 
-Leave an star on github repo its free ( we push updates based on the engagement )
-Repo link: https://github.com/OSL-Works/Steam-Games-Idle 
+/*--------------------------------------------------------------------------------------------- 
+* Original work: Copyright (c) 2020-2021 Refloow All rights reserved.
 
-  Discord Support Server: https://discord.gg/D8WCtDD     (Open an ticket)
+* Code origin: https://github.com/OSL-Works/Steam-Games-Idle
+* Developer name: Veljko Vuckovic
+* Licensed under the MIT License. See LICENSE in the project root for license information.
+* Published License: https://github.com/OSL-Works/Steam-Games-Idle/blob/master/LICENSE
+
+* Contact information:
+  Discord Support Server: https://discord.gg/D8WCtDD
+  Main developer steam: https://steamcommunity.com/id/MajokingGames/ 
+  Mail: refloowlibrarycontact@gmail.com
+
+* Donations:
+  Donate: https://ko-fi.com/refloow
+ --------------------------------------------------------------------------------------------*/
+
+ /* 
+
+// legal advice: PERMISSIONS AND RIGHTS
+
+* License does not prohibit modification, distribution, private/commercial use or sale of copies as long as the original LICENSE file
+ and authors copyright notice are left ass they are in the project files.
+* Copyright notice could be included ones or multiple times within the file.
+* Copyright notice should not be removed even within the larger works (Larger modifications applied).
+* Original file tags cannot be removed without creators exclusive permission.
+* Adding own tags in files is possible in case of modification - even in that case original tags must be kept.
+* Year on the copyright notice breakdown:
+* Generally, the “year of first publication of the work” refers to the year in which the work was first distributed to the public (first year mentioned)
+* Any year after represents the year of added modifications.
+* Copyright cannot expire so therefore you cannot remove copyright notice if its not updated to the latest year.
+* Editing existing copyright notice(s) is also prohibited.
+
+===================================================================================
+Removing copyright notice & distributing, using or selling the software without
+the original license and copyright notice is licence agreement breach and its considered criminal offense and piracy.
+===================================================================================
 
 */
-	
-
-/* 
-  Here is contact info: refloowlibrarycontact@gmail.com
-  main dev steam: https://steamcommunity.com/id/MajokingGames/
-  
-  Donate: https://ko-fi.com/refloow
-
- */
-
 
 // Checking if required modules are properly installed
 
@@ -73,26 +92,4 @@ t = module.exports = {
     }
 }
 
-// Steam-Games-Idle - Bot built by Refloow (-MajokingGames)
-	
-	    
-/*
-
-Want active support and new updates with new features all for free?
-
-Leave an star on github repo its free ( we push updates based on the engagement )
-Repo link: https://github.com/OSL-Works/Steam-Games-Idle 
-
-  Discord Support Server: https://discord.gg/D8WCtDD     (Open an ticket)
-
-*/
-	
-
-/* 
-  Here is contact info: refloowlibrarycontact@gmail.com
-  main dev steam: https://steamcommunity.com/id/MajokingGames/
-  
-  Donate: https://ko-fi.com/refloow
-
- */
 
