28a3df280ac5836b20d2045c612a5a5aed17c1d3
Veljko Vuckovic <59449926+Refloow@users.noreply.github.com>
2021-03-10 18:56:15 +0100

    Added notes

--- app/app.js
+++ app/app.js
@@ -7,7 +7,7 @@
 * Code origin: https://github.com/OSL-Works/Steam-Games-Idle
 * Developer name: Veljko Vuckovic
 * Licensed under the MIT License. See LICENSE in the project root for license information.
-* Published License: https://github.com/OSL-Works/Steam-Games-Idle/blob/master/LICENSE
+* Published License: https://github.com/OSL-Works/Steam-Games-Idle/master/LICENSE
 
 * Contact information:
   Discord Support Server: https://discord.gg/D8WCtDD
@@ -105,3 +105,7 @@ refloowidle.on('loggedOn', () => {
 	infolog.info(`| [IDLE] | GAMES |: Bot started idling for ${config.GameToIdleFor}`);
 });
 
+// Copyright notice:
+
+/* Original work: Copyright (c) 2020-2021 Refloow All rights reserved.
+  Code origin (Free GitHub publish): https://github.com/OSL-Works/Steam-Games-Idle*/
