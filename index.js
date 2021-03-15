eb0080bc5b9abd8301024dbb46422ab3de8a337c
Veljko Vuckovic <59449926+Refloow@users.noreply.github.com>
2021-03-15 17:58:57 +0100

    Added notes

--- index.js
+++ index.js
@@ -54,6 +54,7 @@ try {
 	process.exit(1);
 }
 
+// Importing required files
 const package = require('./package.json');
 const v = package.version; 
 
@@ -74,7 +75,7 @@ console.log('o888o  88o8  88oooo888 o888o   o888o  88ooo88     88ooo88     88   
 console.log('/* Original work: Copyright (c) 2020-2021 Refloow All rights reserved. \n Code origin (Free GitHub publish): https://github.com/OSL-Works/Steam-Games-Idle*/\n'.cyan);
 
 console.log('This bot was developed by MajokingGames'.cyan);
-console.log(`Verision ${v}`.cyan);
+console.log(`Verision v${v}`.cyan);
 console.log('Thanks for choosing us.'.cyan);
 console.log('Loading config file...'.green );
 console.log('Starting bot...'.green );
